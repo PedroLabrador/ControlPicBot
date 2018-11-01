37400da725bcdac94800acbd521f5958ae2395f0
PedroLabrador <pedrolabrador3@outlook.com>
2018-11-01 10:07:54 -0400

    Reordered code

--- bot.py
+++ bot.py
@@ -1,106 +1,144 @@
+# -*- coding: utf-8 -*-
+'''
+Author: Pedro Labrador @SrPedro at Telegram
+'''
 from flask import Flask, request
 import telepot
 import urllib3
 import MySQLdb
 
-proxy_url = "http://proxy.server:3128"
-telepot.api._pools = {
-    'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
-}
-telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
+class controlpicbot(Flask):
+    proxy_url = "http://proxy.server:3128"
+    CONTROL_URL = 'CONTROL_API_URL'
+    secret = "SECRET"
+    API_KEY = "SECRET_API_KEY"
+    WEBHOOK_URL = "WEBHOOK_URL/{}".format(secret)
 
-CONTROL_URL = 'CONTROL_API_URL'
-secret = "SECRET"
-bot = telepot.Bot('SECRET_API_KEY')
-bot.setWebhook("WEBHOOK_URL".format(secret), max_connections=100)
-app = Flask(__name__)
+    def __init__(self, name):
+        super(controlpicbot, self).__init__(name)
+        telepot.api._pools = {'default': urllib3.ProxyManager(proxy_url=self.proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),}
+        telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=self.proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
 
-def get_chat_id(data):
-	chat_id = data['message']['chat']['id']
-	return chat_id
+        self.add_url_rule('/{}'.format(self.secret), view_func=self.command_handler, methods=["POST"])
 
-def get_message(data):
-    message_text = data['message']['text']
-    return message_text
+        self.bot = telepot.Bot(self.API_KEY)
+        self.bot.setWebhook(self.WEBHOOK_URL, max_connections=100)
 
-def get_control_pic_url(message):
-    control_pic_url = CONTROL_URL.format(message)
-    return control_pic_url
 
-def send_message(data, message):
-    chat_id = get_chat_id(data)
-    response = bot.sendMessage(chat_id, message)
-    return response
+    def get_chat_id(self, data):
+        chat_id = data['message']['chat']['id']
+        return chat_id
 
-def send_control_pic(data, message):
-    chat_id = get_chat_id(data)
-    url_pic = get_control_pic_url(message)
-    response = bot.sendPhoto(chat_id, url_pic)
-    return response
 
-def find_name_lastname(data, name, lastname):
-    db = MySQLdb.connect(host="HOST",
-        user="USER",
-        passwd="PASSWD",
-        db="DB")
+    def get_message(self, data):
+        message_text = data['message']['text']
+        return message_text
 
-    cur = db.cursor()
-    query = "SELECT * FROM students WHERE lastname = '{}' AND name = '{}'".format(lastname, name)
-    cur.execute(query)
 
-    found_list = []
+    def get_control_pic_url(self, message):
+        control_pic_url = self.CONTROL_URL.format(message)
+        return control_pic_url
 
-    for row in cur.fetchall():
-        found_list.append(row)
 
-    db.close()
+    def send_message(self, data, message):
+        chat_id = self.get_chat_id(data)
+        response = self.bot.sendMessage(chat_id, message)
+        return response
 
-    return fetch_data(data, found_list)
 
-def fetch_data(data, found):
-    if not found:
-        send_message(data, "No se encuentra a est@ joven en nuestros registros.")
-        return "Failure"
-    else:
-        if len(found) == 1:
-            row = found[0]
-            send_message(data, row[5] + "\n" + row[2] + " " + row[3] + " " + row[0] + " " + row[1] + "\n" + row[4])
-            send_control_pic(data, row[4][1:])
+    def send_control_pic(self, data, message):
+        chat_id = self.get_chat_id(data)
+        url_pic = self.get_control_pic_url(message)
+        response = self.bot.sendPhoto(chat_id, url_pic)
+        return response
+
+
+    def find_name_lastname(self, data, name, lastname):
+        db = MySQLdb.connect(host="HOST", user="USER", passwd="PASSWD", db="DB")
+        cursor = db.cursor()
+        query = "SELECT * FROM users WHERE lastname = '{}' AND name = '{}'".format(lastname, name)
+        cursor.execute(query)
+
+        found_list = []
+
+        for row in cursor.fetchall():
+            found_list.append(row)
+            print(row)
+
+        db.close()
+
+        return self.fetch_data(data, found_list)
+
+
+    def fetch_data(self, data, found):
+        if not found:
+            self.send_message(data, "No se encuentra a est@ joven en nuestros registros.")
+            return "Failure"
         else:
-            send_message(data, "Estos son los resultados:")
-            for row in found:
-                send_message(data, row[5] + "\n" + row[2] + " " + row[3] + " " + row[0] + " " + row[1] + "\n" + row[4])
-    return "Success"
+            if len(found) == 1:
+                row = found[0]
+                self.send_message(data, row[5] + "\n" + row[2] + " " + row[3] + " " + row[0] + " " + row[1] + "\n" + row[4])
+                self.send_control_pic(data, row[4][1:])
+            else:
+                self.send_message(data, "Estos son los resultados:")
+                for row in found:
+                    self.send_message(data, row[5] + "\n" + row[2] + " " + row[3] + " " + row[0] + " " + row[1] + "\n" + row[4])
+        return "Success"
 
-def find_dni(data, dni):
-	response = send_control_pic(data, dni)
-	print (response)
 
-def command_handler(data):
-	line = data['message']['text']
-	tokens = line.split(" ")
+    def find_id(self, data, id):
+        try:
+            response = self.send_control_pic(data, id)
+        except Exception as e:
+            self.send_message(data, "No se encuentra a est@ joven :(")
+        return response
 
-	if '/start' in tokens:
-		send_message(data, "Hola bienvenido!")
-	elif '/ayuda' in tokens:
-		send_message(data, "En Contruccion")
-	elif '/buscar_nombre_apellido' in tokens or '/buscar_nombre_apellido@ControlPicBot' in tokens:
-		if len(tokens) < 3 or len(tokens) > 3:
-			send_message(data, "Recuerda que para buscar por nombre y apellido, debes ingresar el nombre y el apellido")
-			return False
-		status = find_name_lastname(data, tokens[1], tokens[2])
-	elif '/buscar_cedula' in tokens or '/buscar_cedula@ControlPicBot' in tokens:
-		if len(tokens) < 2 or len(tokens) > 2:
-			send_message(data, "Escriba /buscar_cedula seguido de un numero de cedula, joven")
-			return False
-		find_dni(data, tokens[1])
-	else:
-	    print("Por favor intenta usar alguno de nuestros comandos")
-	    #send_message(data, "Por favor intenta usar alguno de nuestros comandos")
 
-@app.route('/{}'.format(secret), methods=["POST"])
-def telegram_webhook():
-    data = request.get_json()
+    def command_start(self, data, tokens):
+        return self.send_message(data, "Hola bienvenid@!")
 
-    command_handler(data)
-    return "OK"
+
+    def command_help(self, data, tokens):
+        return self.send_message(data, "En Contruccion")
+
+
+    def command_buscar_nombre_apellido(self, data, tokens):
+        if len(tokens) < 3 or len(tokens) > 3:
+            self.send_message(data, "Recuerda que para buscar por nombre y apellido, debes ingresar el nombre y el apellido")
+            return "Failure"
+        return self.find_name_lastname(data, tokens[1], tokens[2])
+
+
+    def command_buscar_cedula(self, data, tokens):
+        if len(tokens) < 2 or len(tokens) > 2:
+            self.send_message(data, "Escriba /buscar_cedula seguido de un numero de cedula, joven")
+            return "Failure"
+        return self.find_id(data, tokens[1])
+
+
+    def command_handler(self):
+        data = request.json
+
+        commands = {
+            '/start': self.command_start,
+            '/help': self.command_help,
+            '/buscar_nombre_apellido': self.command_buscar_nombre_apellido,
+            '/buscar_nombre_apellido@ControlPicBot': self.command_buscar_nombre_apellido,
+            '/buscar_cedula': self.command_buscar_cedula,
+            '/buscar_cedula@ControlPicBot': self.command_buscar_cedula
+            }
+
+        if 'entities' in data['message']:
+            line = data['message']['text']
+            tokens = line.split(" ")
+            for i in data['message']['entities']:
+                if 'bot_command' in i['type']:
+                    handler = commands.get(tokens[0], None);
+                    if self.handler:
+                        status = handler(data, tokens)
+                        print (status)
+
+        return "OK"
+
+
+app = controlpicbot(__name__)
